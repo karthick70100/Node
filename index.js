885972664bfd4b7a956f5b41bcf7aa9609681690
karthick70100 <166790786+karthick70100@users.noreply.github.com>
2024-04-12 11:59:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-const mongoose = require('mongoose')
+
 const express = require('express')
 
 app = express()
 
-mongoose.connect('mongodb://localhost:27017/newdb').then(()=>{
-    console.log("Db is Connected")
-})
-console.log("Hello")
 
-const UserSchema = new mongoose.Schema({
-    name:String
-})
-const UserModel = mongoose.model("newcol",UserSchema)
 
 app.get("/",(req,res)=>{
-    UserModel.find({}).then(function(newcol){
-        res.json(newcol)
+      res.write("Hello")
     })
 })
 app.listen(5000) 
